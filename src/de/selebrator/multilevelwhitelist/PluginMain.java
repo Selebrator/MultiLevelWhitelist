e47188c63871b9e0d97ac852399d96c5918ff93a
Selebrator <l.panneke@googlemail.com>
2017-10-03 21:51:24 +0200

    Removed redundant import

--- src/de/selebrator/multilevelwhitelist/PluginMain.java
+++ src/de/selebrator/multilevelwhitelist/PluginMain.java
@@ -12,7 +12,6 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
